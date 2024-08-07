376ca9ad17ac368087018fbc8b756817fabf3865
kien1282003 <occhoever1231@gmail.com>
2024-08-07 10:27:00 +0700

    finish Cau 1

---
+++ 20_2033211363_NguyenTrungKien/Binary_Search.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_2033211363_NguyenTrungKien
+{
+    public class BSKhongDeQuy
+    {
+        public int BinarySearchKhongDeQuy(int[] array, int target)
+        {
+            int low = 0;
+            int high = array.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if (array[mid] == target)
+                {
+                    return mid; // Tìm thấy phần tử
+                }
+                else if (array[mid] < target)
+                {
+                    low = mid + 1; // Tìm kiếm trong nửa mảng bên phải
+                }
+                else
+                {
+                    high = mid - 1; // Tìm kiếm trong nửa mảng bên trái
+                }
+            }
+
+            return -1; // Không tìm thấy phần tử
+        }
+    }
+}
---
+++ 20_2033211363_NguyenTrungKien/Program.cs
@@ -0,0 +1,40 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_2033211363_NguyenTrungKien
+{
+    internal class Search
+    {
+        static void Main(string[] args)
+        {
+            BSKhongDeQuy bsKhongDeQuy = new BSKhongDeQuy();;
+            int[] array = { 11, 2, 7, 5, 4, 23, 22, 5, 6, 1, 99, 76, 21 };
+            Console.Write("\n\nMang chua sap xep: ");
+            foreach (int value in array)
+            {
+                Console.Write(value + " ");
+            }
+            Array.Sort(array);
+            Console.Write("\n\nMang sau khi sap xep: ");
+            foreach (int value in array)
+            {
+                Console.Write(value + " ");
+            }
+            Console.Write("\n\nNhap so tim kiem: ");
+            int target = int.Parse(Console.ReadLine());
+            int indexKhongDeQuy = bsKhongDeQuy.BinarySearchKhongDeQuy(array, target);
+            if (indexKhongDeQuy != -1)
+            {
+                Console.WriteLine($"\nTim thay phan tu {target} tai vi tri {indexKhongDeQuy} ");
+            }
+            else
+            {
+                Console.WriteLine($"\nKhong tim thay phan tu {target}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
