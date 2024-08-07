0a408f2bf01512e139bd61c0f22eb50e8b7cd511
kien1282003 <occhoever1231@gmail.com>
2024-08-07 10:32:52 +0700

    finish Cau 2

---
+++ Cau 2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cau_2
+{
+    internal class Sort
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so luong phan tu n: "); //Nhập số lượng phần tử n
+            int n = int.Parse(Console.ReadLine());
+            int[] array1 = NhapMangRandom(n); //In mảng Random
+            Console.Write("\n\nMang random: ");
+            foreach (int value in array1)
+            {
+                Console.Write(value + " ");
+            }
+            QuickSort sort = new QuickSort();
+            sort.Quicksort(array1, 0, array1.Length - 1);
+            Console.Write("\n\nMang sau khi sap xep la: "); //In mảng sau khi sắp xếp
+            foreach (int value in array1)
+            {
+                Console.Write(value + " ");
+            }
+            Console.ReadKey();
+        }
+        static int[] NhapMangRandom(int n) //Hàm nhập mảng random
+        {
+            Random random = new Random();
+            if (n <= 0)
+            {
+                throw new ArgumentException("Kich thuc mang phai lon hon 0");
+            }
+
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = random.Next(1, 100); //Tạo số ngẫu nhiên trong phạm vi
+            }
+
+            return array;
+        }
+    }
+}
---
+++ Cau 2/QuickSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cau_2
+{
+    public class QuickSort
+    {
+        public void Quicksort(int[] array, int start, int end)
+        {
+            if (start < end) //Nếu như vị trí bắt đầu nhỏ hơn vị trí kết thúc thì thực thi
+            {
+                int divide = Partition(array, start, end); //Chia mảng thành 2 phần
+
+                Quicksort(array, start, divide - 1); //Sắp xếp phần từ vị trí bắt đầu đến vị trí được chia
+                Quicksort(array, divide + 1, end); //Sắp xếp phần tử từ vị trí được chia đến vị trí kết thúc
+            }
+        }
+
+        static int Partition(int[] array, int start, int end)
+        {
+            int pivot = array[end];
+            int i = start - 1;
+
+            for (int j = start; j < end; j++) //Sử dụng vòng lặp for để so sánh phần tử đầu và phần tử "trục" trong mảng
+            {
+                if (array[j] <= pivot) //Nếu phần từ được so sánh và trục nhỏ hơn thì bắt đầu thực hiện hành động swap
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+
+            Swap(array, i + 1, end);
+            return i + 1;
+        }
+
+        static void Swap(int[] array, int i, int j) //Hàm đổi chỗ để sắp xếp các phần tử
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
